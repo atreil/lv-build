52da11b27aa598585b0e2772dede3e93e45fc772
atreil <treilalex@gmail.com>
2021-09-26 16:18:30 -0700

    Add steps for enabilng wireless access

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,3 +2,14 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+touch "${ROOTFS_DIR}/boot/ssh"
+echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=US
+network={
+	ssid="${SSID:?must set SSID}"
+	psk="${PSK:?must set PSK}"
+	scan_ssid=1
+}' > "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf"
+
+echo "0" > tee "${ROOTFS_DIR}/var/lib/systemd/rfkill/*:wlan"
